547df76d8d0a27dc6747dbf053b810880b87e044
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-28 22:50:48 +0100

    Create bash.zsh

---
+++ bash.zsh
@@ -0,0 +1,8 @@
+# Clone
+git clone https://github.com/dp-web4/Synchronism.git
+cd Synchronism
+
+# Explore
+cat Research/SESSION_MAP.md          # Navigate research
+cat Research/discoveries/            # See validated findings
+cat STATUS.md                        # Honest assessment
